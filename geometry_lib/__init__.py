13161aeadefdf890135ce8cfe9cd26bd85409fc5
ToMaTiK <tomatik58@gmail.com>
2025-05-31 15:46:03 +0300

    feature(core): define lib exceptions

--- geometry_lib/__init__.py
+++ geometry_lib/__init__.py
@@ -1,1 +1,13 @@
-# публичные апи, позже будут сделаны
+# пакет 'geometry_lib'
+from .exceptions import ShapeError, InvalidShapeParametersError, NonPositiveValueError, TriangleInequalityError
+
+__version__ = "0.0.1-dev"
+
+# экспорт для пользователей
+__all__ = [
+        'ShapeError',
+        'InvalidShapeParametersError',
+        'NonPositiveValueError',
+        'TriangleInequalityError',
+        '__version__',
+]
